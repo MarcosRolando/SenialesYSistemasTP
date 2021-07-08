846ce8c94d12b1f09b20fc22bf5b3e41a988b9aa
Marcos Rolando <marcosrolando.mr@gmail.com>
2021-07-08 20:08:21 -0300

    Termino ejercicio 3

--- tp.m
+++ tp.m
@@ -58,12 +58,26 @@ ylim([0, 1500])
 % Ejercicio 3
 
 for i = (1:4)
-    [c, lags] = xcorr(audios(:,i), audios(:,i+1), 20);
-    [~, max_index] = max(c);
+    [coeffs, lags] = xcorr(audios(:,i), audios(:,i+1), 20);
+    [~, max_index] = max(coeffs);
     k(i) = -lags(max_index); % Dado que xcorr usa en la definicion x[i + k] pero el enunciado usa x[i - k],
                               % debemos hacer un negado del indice para que sea equivalente a la del enunciado
 endfor
 
 format short e
 
-k / Fs
+disp("\nRetardos obtenidos mediante el primer método (en segundos):\n")
+taus = k / Fs % Retardos entre los microfonos consecutivos
+
+for i = (1:4)
+    dft_curr_mic = fft(audios(:,i));
+    dft_next_mic = fft(audios(:,i+1));
+    Gph = dft_curr_mic .* conj(dft_next_mic) ./ (abs(dft_curr_mic).*abs(dft_next_mic));
+    gph = real(ifft(Gph));
+    [~, max_index] = max(gph(1:100)); % Lo limito a 100 puntos porque el maximo esta dentro de ese rango y si tomo todo
+                                      % agarraria el ruido del final
+    taus(i) = max_index;
+endfor
+
+disp("\nRetardos obtenidos mediante el segundo método (en segundos):\n")
+taus = taus / Fs % Paso de muestras a tiempo
